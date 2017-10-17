33937692f7e01b6a8bae77892f6d72c183d21e7f
Søren Hansen <soer393c@edu.eal.dk>
2017-10-17 12:57:28 +0200

    small changes

--- ProjectWebserviceWebApi/DAL/LogRepository.cs
+++ ProjectWebserviceWebApi/DAL/LogRepository.cs
@@ -12,14 +12,14 @@ namespace ProjectWebServiceWebApi.DAL
     {
         public List<Log> listOfLogs;
         public List<Log> listOfNewLogs;
-        string line;
         object lockObj = new object();
         object lockObjTwo = new object();
+        string line;
         public LogRepository()
         {
+            
             listOfLogs = ReadFile();
-
-            //new Thread(() => UpdateLogList()).Start();
+            new Thread(() => UpdateLogList()).Start();
         }
 
         public string[] SeperateString(string input)
@@ -29,76 +29,64 @@ namespace ProjectWebServiceWebApi.DAL
 
         public void UpdateLogList()
         {
-            bool running = true;
             
-            while (running)
+            while (true)
             {
-                int oldListCount = listOfLogs.Count;
-
-                listOfNewLogs = ReadFile();
-                
-                listOfNewLogs.Add(AddNewLines());
-
-                if (listOfNewLogs.Count > listOfLogs.Count)
+                lock (lockObj)
                 {
-                    lock (lockObj)
+                    listOfNewLogs = ReadFile();
+
+                    for (int i = 0; i < listOfNewLogs.Count - listOfLogs.Count; i++)
                     {
-                        try
-                        {
-                            for (int i = oldListCount; i < listOfNewLogs.Count; i++)
-                            {
-                                listOfLogs.Add(listOfNewLogs[i]);
-                            }
-                        }
-                        catch (Exception)
-                        {
-                            return;
-                        }
+                        listOfLogs.Add(listOfNewLogs[i + 1]);
                     }
+                    Thread.Sleep(5000);
                 }
-                Thread.Sleep(5000);
+
             }
         }
 
         public List<Log> FindLogByName(string nameToFind)
         {
+            //UpdateLogList();
             return listOfLogs.FindAll(x => x.Name == nameToFind);
         }
 
         public List<Log> FindLogById(string idToFind)
         {
+            //UpdateLogList();
             return listOfLogs.FindAll(x => x.Id == idToFind);
         }
 
         public List<Log> GetFullList()
         {
+            //UpdateLogList();
             return listOfLogs;
         }
 
         public Log AddNewLines()
         {
-            string[] ss = new string[8] {"20:20:20", "101010", "alert", "name", "department", "resident", "", "registered" };
+            string[] ss = new string[8] { "20:20:20", "101010", "alert", "name", "department", "resident", "", "registered" };
             return new Log(ss);
         }
 
         public List<Log> ReadFile()
         {
-            lock (lockObjTwo)
+            List<Log> output = new List<Log>();
+            string fileSource = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/logfil.txt";
+            string target = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/logfil_kopi.txt";
+            File.Copy(fileSource, target);
+
+            System.IO.StreamReader fileKopi = new System.IO.StreamReader(target);
+
+            while ((line = fileKopi.ReadLine()) != null)
             {
-                List<Log> output = new List<Log>();
-                System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Søren\Source\Repos\ProjectWebServiceApi\ProjectWebServiceApi\DAL\logfil.txt");
-                file.ReadLine();
-
-                while ((line = file.ReadLine()) != null)
-                {
-                    string[] dataSeperated = SeperateString(line);
-                    output.Add(new Log(dataSeperated));
-                }
-                file.Close();
-
-                return output;
+                string[] dataSeperated = SeperateString(line);
+                output.Add(new Log(dataSeperated));
             }
-
+            fileKopi.Close();
+            File.Delete(target);
+            return output;
         }
     }
 }
